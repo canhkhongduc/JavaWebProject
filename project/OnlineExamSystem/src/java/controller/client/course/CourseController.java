6f7cc9b8f2423177d869126ee52a44eff0a6ee3e
MinhCT <minhct1997@gmail.com>
2017-03-21 23:06:46 +0700

    Added client course list controller

--- project/OnlineExamSystem/src/java/controller/client/course/CourseController.java
+++ project/OnlineExamSystem/src/java/controller/client/course/CourseController.java
@@ -3,11 +3,14 @@
  */
 package controller.client.course;
 
+import dao.CourseManager;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import model.Course;
 import util.servlet.ManagedServlet;
 
 /**
@@ -19,7 +22,19 @@ public class CourseController extends ManagedServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        getCorrespondingViewDispatcher().forward(request, response);
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+    
+    protected void processRequest(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        CourseManager cm = new CourseManager();
+        List<Course> courses = cm.getAllCourses();
+        req.setAttribute("courses", courses);
+        getCorrespondingViewDispatcher().forward(req, resp);
     }
     
 }
